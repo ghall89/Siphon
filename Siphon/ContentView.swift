a9e255dbb6e1d9355288059c0cec16a4c2df0c94
Graham Hall <graham@ghall.dev>
2024-01-16 19:52:01 -0500

    Initial Commit

---
+++ Siphon/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Siphon
+//
+//  Created by Graham Hall on 1/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Siphon/SiphonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SiphonApp.swift
+//  Siphon
+//
+//  Created by Graham Hall on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct SiphonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
